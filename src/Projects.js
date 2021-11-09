f6d7c12dc93f42f2a8ca5edad4025f7287c818bf
Jason <jasonabimbola@msn.com>
2021-11-09 14:56:19 +0000

    autoplay true

--- src/Projects.js
+++ src/Projects.js
@@ -36,7 +36,7 @@ const Project = ({ fullpageApi }) => {
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 5000,
     cssEase: 'linear',
   }
