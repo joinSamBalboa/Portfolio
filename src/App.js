ee1e534bba904978a63c8916844644c82ba34688
Jason <jasonabimbola@msn.com>
2021-11-17 10:54:22 +0000

    react-ga

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Fullpage from './Fullpage'
+import ReactGA from 'react-ga'
+ReactGA.initialize('UA-212975831-2')
+ReactGA.pageview(window.location.pathname + window.location.search)
 
 function App() {
   return <Fullpage />
